2c4807482ec0d836a465f8b644ca8713149109f0
Lukas Wirth <lukastw97@gmail.com>
2024-12-16 14:21:51 +0100

    internal: port `SyntaxContext`; macro expansion to new salsa.
    This commit consists of several changes:
    - Reimplement `SyntaxContext` using new salsa's self-referential interning
    - fix `SyntaxContext`'s `StructData` Hash and PartialEq impls
    - fix `SyntaxContext` Debug impl
    - Do not store IDs of inputs in the inputs themselves
    - the removal of `ra-salsa`

--- crates/hir-expand/src/builtin/quote.rs
+++ crates/hir-expand/src/builtin/quote.rs
@@ -276,8 +276,8 @@ mod tests {
         assert_eq!(quoted.to_string(), "hello");
         let t = format!("{quoted:#?}");
         expect![[r#"
-            SUBTREE $$ 937550:0@0..0#Id(0) 937550:0@0..0#Id(0)
-              IDENT   hello 937550:0@0..0#Id(0)"#]]
+            SUBTREE $$ 937550:0@0..0#4294967039 937550:0@0..0#4294967039
+              IDENT   hello 937550:0@0..0#4294967039"#]]
         .assert_eq(&t);
     }
 
